8cefcf0b79930232ac57abd7b68a07bff6bb541b
github username <github email address>
2019-10-18 16:01:38 -0500

    display matches on chat container

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,6 @@
 class MatchesController < ApplicationController
+    def show
+        matches = Match.all.where('user_one_id = ? or user_two_id = ?', params[:id], params[:id])
+        render json: matches.as_json(:include => { :user_one => {:only => [:name, :id]} , :user_two => {:only => [:name, :id]}} )
+    end
 end
