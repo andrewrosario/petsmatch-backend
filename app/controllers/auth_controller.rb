494325268a44873184ccf2bf0cbf8d814c7d9392
Jason <jas0nle@yahoo.com>
2019-10-21 14:35:15 -0500

    login auth functionality finished

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,18 +1,20 @@
 class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      # encode token comes from ApplicationController
-      token = encode_token({ user_id: @user.id })
-      render json: { user: @user, jwt: token }, status: :accepted
-    else
-      render json: { message: 'Invalid username or password' }, status: :unauthorized
+    def create
+        @user = User.find_by(email: params[:auth][:email])
+        if @user && @user.authenticate(params[:auth][:password])
+          # encode token comes from ApplicationController
+          token = encode_token({ user_id: @user.id })
+          render json: { user: @user, jwt: token }, status: :accepted
+        else
+          render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
     end
 
     private
 
     def user_login_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :password)
     end
 end
